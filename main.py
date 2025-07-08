f0c174d490b3d4de27638a361a28aa2ff1b2e013
xsamueljr <samueljiro.alfa@gmail.com>
2025-07-08 15:58:29 +0200

    feat: dynamic choosing between a real and a fake github service

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from github.infrastructure.real_github_service import RealGitHubService
 from maths.application.dtos.quadratic_equation_result import QuadraticEquationResult
 from maths.application.quadratic_equation import SolveQuadraticEquationUseCase
 from maths.application.square_root import CalculateSquareRootUseCase
+from shared.infrastructure.env import ENV, AppEnvType
+from tests.mocks import MockGitHubService
 
 mcp = FastMCP("MCP example")
 
@@ -20,7 +22,15 @@ def square_root(x: int) -> float:
 def quadratic_equation(a: int, b: int, c: int) -> QuadraticEquationResult:
     return SolveQuadraticEquationUseCase().run(a, b, c)
 
-github_controller = GitHubMCPController(RealGitHubService())
+if ENV.APP_ENV == AppEnvType.PRODUCTION:
+    print("using real")
+    github_service = RealGitHubService()
+else:
+    print("using mock")
+    github_service = MockGitHubService()
+
+
+github_controller = GitHubMCPController(github_service)
 mcp.tool(github_controller.get_user_info)
 
 if __name__ == "__main__":
